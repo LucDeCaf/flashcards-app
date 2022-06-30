872fe0d0ea9084e91038ecceef73a059784eb34f
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-06-30 09:11:14 +0200

    Reformatted afr_words.csv + changed text color for Eng words

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import random
 class FlashcardApp:
     def __init__(self):
         # -------------------- Initialize Variables -------------------- #
-        # Read Afrikaans words file and save values to list of dictionaries called afr_words
-        with open('afr_words.csv', 'r') as datafile:
-            afr_words_df = pd.read_csv(datafile)
-            # Set the first column to the index column
-            afr_words_df.set_index('Unnamed: 0', inplace=True)
-            self.afr_words = afr_words_df.to_dict(orient='records')
+        # If this is the first time running the program, get information from afr_words.csv.
+        # Otherwise, read from file words_to_learn.csv which will be created at the end of the program
+        try:
+            with open('words_to_learn.csv', 'r') as data:
+                self.afr_words = pd.read_csv(data).to_dict(orient='records')
+        except FileNotFoundError:
+            with open('afr_words.csv', 'r') as data:
+                self.afr_words = pd.read_csv(data).to_dict(orient='records')
 
         # Initialize random_word as random English/Afrikaans word pair
         self.random_word = random.choice(self.afr_words)
@@ -41,7 +43,7 @@ class FlashcardApp:
 
         # Add Yes button
         self.yes_btn = Button(image=yes_img, bg='#B1DDC6', borderwidth=0, highlightthickness=0,
-                              command=self.show_afr_word)
+                              command=self.correct_word)
         self.yes_btn.grid(row=2, column=1)
 
         # Add No button
@@ -57,20 +59,28 @@ class FlashcardApp:
         # Get random word from afr_words
         self.random_word = random.choice(self.afr_words)
 
-        # Show random Afrikaans word
+        # Get random Afrikaans word
         random_afr_word = self.random_word['Afrikaans']
-        self.canvas.itemconfig(self.language_txt, text='Afrikaans')
-        self.canvas.itemconfig(self.current_word_txt, text=random_afr_word)
+
+        # Display chosen Afrikaans word
+        self.canvas.itemconfig(self.language_txt, text='Afrikaans', fill='#000000')
+        self.canvas.itemconfig(self.current_word_txt, text=random_afr_word, fill='#000000')
         self.card_img.config(file='images/card_front.png')
 
         # Wait 3 seconds, then show english translation
         self.window.after(ms=3000, func=self.show_eng_word)
 
     def show_eng_word(self):
+        # Get translation of random Afrikaans word
         random_eng_word = self.random_word['English']
-        self.canvas.itemconfig(self.language_txt, text='English')
-        self.canvas.itemconfig(self.current_word_txt, text=random_eng_word)
+
+        # Show chosen English word
+        self.canvas.itemconfig(self.language_txt, text='English', fill='#FFFFFF')
+        self.canvas.itemconfig(self.current_word_txt, text=random_eng_word, fill='#FFFFFF')
         self.card_img.config(file='images/card_back.png')
+
+    def correct_word(self):
+        self.show_afr_word()
 
 
 application = FlashcardApp()
